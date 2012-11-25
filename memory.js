fd3cd46109b0d3585f8c6c0036f0e514a5872895
Kenya <kenya.vs@gmail.com>
2012-11-25 16:22:34 -0500

    lots of refactoring and editing

--- memory.js
+++ memory.js
@@ -1,42 +1,71 @@
-//var data = [{"text":"Twitter", "count":2}, {"text":"Facebook", "count":2},{"text":"Spotify", "count":2}, {"text":"Grooveshark", "count":2}, {"text":"Pandora", "count":2}, {"text":"Instagram", "count":2}];
-
+/**
+ 	* Adds click event listeners to the "play" and "play again" buttons when the window loads.
+ */
 window.onload = function(){
-	//add event listeners to the button
-	var play_button = document.getElementsByClassName("button")[0];
+	//var play_button = document.getElementsByClassName("button")[0];
+	//TODO: try abstracting things out and making the following a reusable function
+	//QUESTION: is it advisable to just give the input elements id values vs doing all that i'm doing below(in the name of only having classes)?
+	var msg_obj = new Message();
+	var play_button='';
+	var username_element_inputs = document.getElementById("username").getElementsByTagName("button");
+			var i=0;
+			var found = false;
+
+			while(i<username_element_inputs.length && (!found)){
+				if(username_element_inputs[i].classList.contains("button")){
+					play_button = username_element_inputs[i];
+					found = true;
+				}			
+				i++;
+			}
+
 	play_button.addEventListener("click", function(e){
-		var username = document.getElementsByClassName("username-input")[0].value;
-		ajax(username);
+
+		//if there's a username make ajax request
+		try{
+			//var username = document.getElementsByClassName("username-input")[0].value;
+			var username='';
+			var username_element_inputs = document.getElementById("username").getElementsByTagName("input");
+			var i=0;
+			var found = false;
+
+			while(i<username_element_inputs.length && (!found)){
+				if(username_element_inputs[i].classList.contains("username-input")){
+					username = username_element_inputs[i].value;
+					found = true;
+				}			
+				i++;
+			}
+
+			if(username==''){
+				throw new UserException("Must enter username to play.");
+			}
+			else{
+				msg_obj.getMessageDrawer().classList.add("hide");
+				//document.getElementById("message-drawer").style.display = 'none';
+				executeAjaxHandler(username);
+			}
+		}
+		catch(err){
+			console.log(err.name);
+			/*HERE*/msg_obj.getMessageDrawer().getElementsByClassName("message-text")[0]
+			/*HERE*/document.getElementsByClassName("message-text")[0].innerHTML = err.msg;
+			msg_obj.getMessageDrawer().classList.remove(err.class_name);//QUESTION: is this line absolutely unecessary? is it better to do the line below? i added this line to avoid accessing the dom so much...
+		}
 	}, false);
 
-	var play_again = document.getElementsByClassName("play-again")[0];
+	var play_again = document.getElementById("play-again");
 	play_again.addEventListener("click", function(e){
 		utilities.clearInputs();
 		location.reload();
 	}, false);
 }
 
+//QUESTION: why doesn't jquery ajax module recognize the anonymous version of this function?
 function jsoncallback(json){
-	console.log(json.length);
-	var shift = true;
-	var json_tmp = [];
-	var json_new = [];
-
-	/*shuffle tweets*/
-	//copy tweets into tmp array
-	/*for(var i=0; i<json.length; i++){
-		json_tmp.push(json[i]);
-	}
-
-	for(var i=0; i<json.length; i++){
-		if(shift){
-			json_new.push(json_tmp.shift());
-			shift=false;
-		}
-		else{
-			json_new.push(json_tmp.pop());
-			shift=true;
-		}
-	}*/
+	var MAX_LENGTH = 6;
+	var txt='';
+	var data=[];
 
 	for (var i = json.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -44,13 +73,7 @@ function jsoncallback(json){
         json[i] = json[j];
         json[j] = temp;
     }	
-
-	MAX_LENGTH = 6;
-	var txt='';
-	var data=[];
 	
-	//if there's data, and it's valid, do the following...
-
 	if(json.length>=MAX_LENGTH){
 	   	for(var i=0; i<MAX_LENGTH; i++){
 	    	txt = json[i].text;
@@ -59,14 +82,14 @@ function jsoncallback(json){
 	    	data.push(obj);
 	  	}
 	  	board.load(data, MAX_LENGTH);
-	  	document.getElementsByClassName("username")[0].style.display = 'none';
-	  	document.getElementsByClassName("div-table")[0].style.display = "table";
+	  	document.getElementById("username").classList.toggle("hide");
+	  	document.getElementById("board").classList.remove("hide");
+	  	document.getElementById("board").classList.add("show-table");
   	}
   	else if(json.error=="Not authorized"){
 		console.log('This user is private. Choose another username');   	
 	}
-	else if(json.errors!="")
-	{
+	else if(json.errors!=""){
 		console.log("This user doesn't exist. Choose another username");
 	}
   	else{
@@ -76,35 +99,52 @@ function jsoncallback(json){
 }
 
 
-var ajax = function(username){
+var executeAjaxHandler = function(username){
 	var url = "https://api.twitter.com/1/statuses/user_timeline.json?screen_name="+username+"&count=20";
+
 	$.ajax({
-		  url: url,
-		  dataType: "jsonp",
-		  jsonp : "callback",
-		  jsonpCallback: "jsoncallback"
-	});
+			  url: url,
+			  dataType: "jsonp",
+			  jsonp : "callback",
+			  jsonpCallback: "jsoncallback"
+		});
+}
+
+var Message = function(){
+	var message_drawer = document.getElementById("message-drawer");
+	this.message_drawer = message_drawer;
+}
+
+Message.prototype = {
+	getMessageDrawer:function(){
+		return this.message_drawer;
+	}
+}
+
+var UserException = function(msg){
+	this.msg = msg;
+	this.name = "UserException";
+	this.class_name = "hide";
 }
 
 var utilities = {
 	clearInputs:function(){
-		//this should be made to be more general, but for now...
-		document.getElementsByClassName("username-input")[0].value = '';
+		var input_elements = document.getElementsByTagName("input");
+
+		for(var i = 0; i<input_elements.length; i++){
+			input_elements[i].value = '';
+		}		
 	}
 }
 
-var error = {
-
-}
-
-var board ={
+var board = {
 	click_num:0,
 	card_count:0,
 	current_clicks:[],
 	game_board:'',
 	cards:[],
-	board_length:4,
-	board_width:3,
+	BOARD_LENGTH:4,
+	BOARD_WIDTH:3,
 	number_of_matches:0,
 
 	/*loads the game board with data*/
@@ -117,12 +157,12 @@ var board ={
 		
 		this.game_board = document.getElementById("board");//would be better to actually access this by class name?
 
-		for(var i = 0; i<this.board_length; i++){
+		for(var i = 0; i<this.BOARD_LENGTH; i++){
 			var row = document.createElement("div");
 			row.className = "div-row";
 			this.game_board.appendChild(row);
 
-			for(var j = 0; j<this.board_width; j++){
+			for(var j = 0; j<this.BOARD_WIDTH; j++){
 				//loop until a cell is filled with an item from the data array
 				while(filled_cell){
 				
@@ -131,8 +171,7 @@ var board ={
 					random_num = board.getRandomNum(data);
 
 					if(data[random_num]["count"]>0){
-						var new_card = new card(data[random_num]["text"], "row"+j);
-						//this.game_board.appendChild(new_card.getCardMarkup());
+						var new_card = new Card(data[random_num]["text"]);
 						row.appendChild(new_card.getCardMarkup());
 						this.card_count++;
 						this.cards.push(new_card);
@@ -145,29 +184,25 @@ var board ={
 			}
 		}
 
-		/*TOM! "this" is being assigned/defined globally!*/
 		this.game_board.addEventListener("click", function(e){
 			if(self.click_num==2){
 				self.compareClicks();
 				
 				//pause before resetting clicks...
-				//reset clicks
 				setTimeout(function(){
 					self.resetClicks();
 
 					//if number of matches found == MAX_LENGTH, the game has ended, ask to reload board...
 					if(board.number_of_matches==board.max_length){
-
-						document.getElementsByClassName("username")[0].style.zIndex = -1;
-						document.getElementsByClassName("play-again")[0].style.display = 'inline';
-
+						document.getElementById("username").classList.toggle("z-index");
+						document.getElementById("play-again").classList.toggle("hide");
 					}
 				}, 500);
 			}
 		})
 	}, 
 
-	/*returns a randomly generated number btwn the valuse of 0 and the length of the data array*/
+	/*returns a randomly generated number btwn the values of 0 and the length of the data array*/
 	getRandomNum: function (){
 		return Math.floor((Math.random()*(this.max_length)));
 	},
@@ -176,7 +211,6 @@ var board ={
 			//gray out cell and disable click/remove click event listener 
 			this.current_clicks[i].target.classList.remove("on");
 			this.current_clicks[i].target.classList.add("off");
-			//this.game_board.removeChild(this.current_clicks[i].target);
 		}	
 		this.click_num = 0;
 		this.current_clicks = [];
@@ -204,7 +238,6 @@ var board ={
 			for(var i = 0; i<this.current_clicks.length; i++){
 				var target = this.current_clicks[i];
 				target.target.addEventListener("click", board.cards[target.target.id].listener, false);
-				//target.target.addListener(board.cards[target.target.id].listener);
 				// QUESTION: is there anyway to obtain the object that the markup is attached to?
 			}
 			console.log("Welp :/");
@@ -216,17 +249,15 @@ var board ={
 			//gray out cell and disable click/remove click event listener 
 			this.current_clicks[i].target.classList.remove("on");
 			this.current_clicks[i].target.classList.add("removed");
-			//this.game_board.removeChild(this.current_clicks[i].target);
 		}
 	}
 
 }
 
 
-var card = function(text, class_name) {
+var Card = function(text) {
 		var div = document.createElement("div");//should div have this prepended on it?
 		div.innerHTML = text;
-		//div.className = "divCell";//class_name;
 		div.classList.add("div-cell");
 		div.classList.add("off");
 		div.id = board.card_count;
@@ -256,135 +287,9 @@ var card = function(text, class_name) {
 		this.markup.addEventListener("click", this.listener, false);
 }
 
-card.prototype = {
-	settext: function(text){
-		this.card_name = text;
-	},
-
-	getName: function(){
-		return this.card_name;
-	},
-
-	/*TODO: remove this b/c not using. or try to figure out how to make it reusable*/
-	addListener: function(){
-		console.log("in add listener");
-		this.markup.addEventListener("click", this.listener, false);
-	},
-
+Card.prototype = {
 	getCardMarkup: function(){
 		return this.markup
 	}
 }
 
-//QUESTION: ok, so what happens here? I have an event listener on both the board and the card/div...what happens first? so it seems like the card event listener fires first. why is that?
-		/*NOTE: this is the element that has added the event listener, div in this case
-					e is the mouse event
-					self is the object passed in outside/around/surrounding this event listener
-		*/
-		
-		//QUESTION: why doesn't this work? but the technique above does?
-		/*div.addEventListener("click", function(e){
-			board.incrementClicks();
-			board.recordClick(name, e);//why is it that this.text...or self.text returns the last item to be created? also, how does this event listener know how to reference "text"...the text assigned to the div?
-			this.removeEventListener("click", function(e2){
-				board.incrementClicks();
-				board.recordClick(text, e2);		
-			})
-		});*/
-
-
-
-/*WHY CAN'T I DO THIS AJAX CALL WITHIN THE FUNCTION INIT..OR WITHIN WINDOW.LOAD?..ALSO, HOW CAN THIS BE DONE WITHOUT USING JQUERY? I.E. USING XMLHTTPREQUEST ETC?
-var data_new = {
-	init: function(){
-		
-		var url = "https://api.twitter.com/1/statuses/user_timeline.json?screen_text=djlindsey&count=1";
-		$.ajax({
-		  url: url,
-		  dataType: "jsonp",
-		  jsonp : "callback",
-		  jsonpCallback: "jsonpcallback"
-		  });
-
-		function jsonpcallback(json) {
-
-		   for (var i=0; i<data.length; i++){ 
-		    console.log(json[i].text);
-		  }
-		}
-	}
-}*/
-
-
-/*var data_new = {
-
-	ajax: function(){
-		console.log("this happended");
-		var xmlhttp;
-		if(window.XMLHttpRequest)
-		{	// code for IE7+, Firefox, Chrome, Opera, Safari
-			xmlhttp=new XMLHttpRequest();
-		}
-		else
-		{	// code for IE6, IE5
-		  	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		
-		xmlhttp.onreadystatechange=function()
-		{
-			if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		    {
-		    	document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-		    }
-		}
-		
-		xmlhttp.open("GET","https://api.twitter.com/1/statuses/user_timeline.json?screen_name=djlindsey&count=1",true);
-		xmlhttp.send();
-	},
-
-	cross_ajax: function(){
-		var invocation = new XMLHttpRequest();
-		var url = 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name=djlindsey&count=1';
-     
-		//function callOtherDomain(){
-		  if(invocation) {
-		    invocation.open('GET', url, true);
-		    invocation.withCredentials = true;
-		    //invocation.onreadystatechange = handler;
-
-		    invocation.onreadystatechange = function()
-			{
-				if (invocation.readyState==4 && invocation.status==200)
-		    	{
-		    		document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-		    	}
-			}
-		    	invocation.send(); 
-		  }*/
-
-		
-		/*var invocation = new XMLHttpRequest();
-	    var url = 'http://aruner.net/resources/access-control-with-post-preflight/';
-	    var invocationHistoryText;
-	    var body = '<?xml version="1.0"?><person><name>Arun</name></person>';
-	    
-	    function callOtherDomain(){
-	        if(invocation)
-	        {
-	            invocation.open('POST', url, true);
-	            invocation.setRequestHeader('X-PINGARUNER', 'pingpong');
-	            invocation.setRequestHeader('Content-Type', 'application/xml');
-	            invocation.onreadystatechange = handler;
-	            invocation.send(body); 
-	        }
-	        else
-	        {
-	            invocationHistoryText = "No Invocation TookPlace At All";
-	            var textNode = document.createTextNode(invocationHistoryText);
-	            var textDiv = document.getElementById("textDiv");
-	            textDiv.appendChild(textNode);
-	        }
-        
-    	}*/
-	/*}
-}*/
